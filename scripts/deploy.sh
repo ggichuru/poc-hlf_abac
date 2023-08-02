184ad40d19926815363f5259d3f11861e0116832
ggichuru <ggichuru.dev@gmail.com>
2023-08-02 23:42:43 +0300

    [ script ] -> deploy chaincode

---
+++ scripts/deploy.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+set -e
+
+INFO="ℹ️ "
+IDEA="💡"
+DONE="✅"
+ERR="❌"
+ABOVE="⬆️"
+NOT_FOUND="❗"
+
+cc_name="abac-asset"
+cc_version=${1:-"1"}
+cc_sequence=${2:-"1"}
+
+echo "${INFO} --> switching dir to ../test-network"
+cd ../test-network
+echo 
+
+setup_env() {
+
+    # Commands required to aid in network interaction
+    echo
+    echo "${INFO} --> setting up env variabales ENV VARIABLES :"
+  
+    export PATH=${PWD}/../bin:$PATH
+    export FABRIC_CFG_PATH=$PWD/../config/
+
+
+    # Environment variables for Org1
+    export CORE_PEER_TLS_ENABLED=true
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+
+    echo
+    echo "${INFO} ---> DEPLOYING CHAINCODE :"
+    echo "               -ccn [chaicodeName]        ==      $cc_name"
+    echo "               -ccp [chaicodePackage]     ==      ../contracts/$cc_name"
+    echo "               -ccp [chaicodeLanguage]    ==      go"
+    echo 
+
+}
+
+
+
+check_contianer() {
+    CNAME=$1
+    if [ "$(docker ps -qa -f name=$CNAME)" ]; then
+        echo "${INFO} ::: Found container - $CNAME"
+        if [ "$(docker ps -q -f name=$CNAME)" ]; then
+            echo "${INFO} ::: Stopping running container - $CNAME"
+            docker stop $CNAME;
+        fi
+        echo "${INFO} ::: Removing stopped container - $CNAME"
+        docker rm $CNAME;
+    fi
+}
+
+deploy_ccaas() {
+    org1="peer0org1_insurance_ccaas"
+    org2="peer0org2_insurance_ccaas"
+    
+    # stop and remove containers if any
+    check_contianer $org1
+    check_contianer $org2
+    echo
+
+    # Deploy chaincode as a service
+    echo "${INFO} --> Deploying chaincode as service"
+    ./network.sh deployCCAAS -ccn $cc_name -ccp ../chaincode/$cc_name/  -ccl go -ccv $cc_version -ccs $cc_sequence
+}
+
+deploy_cc() {
+    # Deploy the chaincode
+    echo "${INFO} --> Deploying chaincode"
+    ./network.sh deployCC -ccn $cc_name -ccp ../chaincode/$cc_name/  -ccl go -ccv $cc_version -ccs $cc_sequence
+
+}
+
+deploy_chaincode() {
+    # get chaincode sequence
+    sequence=$(peer lifecycle chaincode queryapproved -C mychannel -n $cc_name --output json | jq -r '.sequence')
+    
+    if [[ "$sequence" -ge 1 ]]; then
+        #increment chaincode version and sequence
+        let cc_version=sequence+1
+        let cc_sequence=sequence+1
+    fi
+
+    # Deploy the chaincode
+    deploy_cc
+
+    #deploy Chaincode as a service
+    # deploy_ccaas
+
+    if [ $? -ne 0 ]; then
+        echo "${ERR} Error Deploying chaincode. ${IDEA} Check logs above ${ABOVE}"
+        exit 1
+    fi
+
+}
+
+copy_testnet_orgs () {
+    if [ -d "../application/test-network" ]; then
+        echo "${INFO} Removing test folder"
+        echo
+        rm -rf ../application/test-network
+        wait
+    fi
+    
+    echo "${INFO} Copying Organizations to bcclient"
+    echo
+        
+    cp -R . ../application/test-network
+    echo "${DONE} Copying Organizations to bcclient"  
+}
+
+
+
+setup_env
+deploy_chaincode
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,7 +16,7 @@ install_fabric() {
         exit 1
     fi
     
-    mkdir ../_cfg
+    # mkdir ../_cfg
 
     echo
     echo "${DONE} Done Installing HLF"
@@ -28,14 +28,14 @@ install_fabric() {
 setup_testnet () {
 
     # Check if the test-network folder exists and delete it
-    if [ ! -d "test-network/" ]; then
+    if [ ! -d "../test-network" ]; then
         echo
         echo "${INFO} -> test-network notFound."
         echo "${INFO} -> Setting up testnet folder"
         echo
 
         # get the test nework folder from fabric-samples repo
-        cp -R fabric-samples/test-network ../_cfg/test-network
+        cp -R fabric-samples/test-network ../test-network
         if [ $? -ne 0 ]; then
             echo "${ERR} Error setting up test-network. ${IDEA} Check logs"
             exit 1
@@ -53,5 +53,25 @@ setup_testnet () {
     echo ""
 }
 
+start_testnet() {
+    echo
+    echo "${INFO} --> starting testnetwork with CA enabled"
+
+    cd ../test-network
+
+    echo "${INFO} -> pulling down any fabric network running"
+    ./network.sh down
+
+
+    echo "${INFO} +> ${IDEA} STARTING THE TEST NETWORK ${IDEA} "
+    echo "  -> we are creating a channel called 'mychannel' whilst enabling couchDB and Certificate Authorities"
+
+    ./network.sh up createChannel -ca -c mychannel -s couchdb
+
+    echo "${DONE} configuring HLF-Testnet"
+    echo ""
+}
+
 install_fabric
 setup_testnet
+start_testnet
